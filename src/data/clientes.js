578994a2cb9f7fc6ef1237221f78cb4fc8c9ccee
juanesgv <juanesgarviedma23@gmail.com>
2024-01-26 15:34:08 -0500

    implementación de crear un nuevo cliente con json-server

--- src/data/clientes.js
+++ src/data/clientes.js
@@ -1,52 +1,24 @@
-export async function obtenerClientes() {
+export async function obtenerClientes (){
+    const url = 'http://localhost:3000/clientes'
     const respuesta = await fetch(import.meta.env.VITE_API_URL)
     const resultado = await respuesta.json()
+
     return resultado
 }
 
-export async function obtenerCliente(id) {
-    const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`)
-    const resultado = await respuesta.json()
-    return resultado
-}
+export async function agregarCliente(datos){
 
-export async function agregarCliente(datos) {
     try {
         const respuesta = await fetch(import.meta.env.VITE_API_URL, {
             method: 'POST',
             body: JSON.stringify(datos),
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'aplication/json'
             }
         })
         await respuesta.json()
     } catch (error) {
         console.log(error)
     }
-}
 
-export async function actualizarCliente(id, datos) {
-    try {
-        const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`, {
-            method: 'PUT',
-            body: JSON.stringify(datos),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        await respuesta.json()
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export async function eliminarCliente(id) {
-        try {
-        const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`, {
-            method: 'DELETE'
-        })
-        await respuesta.json()
-    } catch (error) {
-        console.log(error)
-    }
 }
